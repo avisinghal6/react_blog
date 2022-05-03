976815119febfad711dfd29ac96c49ea685abf8b
avi singhal <97785770+avisinghal6@users.noreply.github.com>
2022-05-03 18:26:49 +0530

    initial setup

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+  apiKey: "AIzaSyAdea1qS3btWNPitN3b95C2WYhqZSjRbWU",
+  authDomain: "react-hook-blog-c5056.firebaseapp.com",
+  projectId: "react-hook-blog-c5056",
+  storageBucket: "react-hook-blog-c5056.appspot.com",
+  messagingSenderId: "359454347558",
+  appId: "1:359454347558:web:0fcf3ba25152ec547ef1e7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore= firebase.firestore();
