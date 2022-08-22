acd34c3da3d38aad5c9c4a2e397975f1abc4be55
Avi Singhal <avisinghal6@gmail.com>
2022-08-21 23:30:43 -0500

    created post using custom hooks

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,10 +1,11 @@
-import {useState} from 'react';
+//import {useState} from 'react';
 import {firestore} from '../firebase';
+import {useFormInput} from '../hooks';
 function CreatePost() {
 
-  const [title, setTitle]=useState();
-  const [content, setContent]=useState();
-  const [subTitle, setSubTitle]=useState();
+  const title=useFormInput('');
+  const subTitle=useFormInput('');
+  const content=useFormInput('');
 
   function handleSubmit(e){
     e.preventDefault();
@@ -13,9 +14,9 @@ function CreatePost() {
     console.log('content',content);
 
     firestore.collection('posts').add({
-      title,
-      content,
-      subTitle,
+      title:title.value,
+      content:content.value,
+      subTitle:subTitle.value,
       createAt: new Date(),
     });
   }
@@ -26,17 +27,18 @@ function CreatePost() {
           <form onSubmit={handleSubmit}>
             <div className="form-field">
               <label> Title</label>
-              <input value={title} onChange={(e)=> setTitle(e.target.value)}/>
+              <input {...title}/> 
+              {/* {...title}(it is called spread operator) will convert to value={title} onChange={(e)=> setTitle(e.target.value)} */}
             </div>
 
             <div className="form-field">
               <label> Sub Title</label>
-              <input value={subTitle} onChange={(e)=> setSubTitle(e.target.value)}/>
+              <input {...subTitle}/>
             </div>
 
             <div className="form-field">
               <label>Content</label>
-              <textarea value={content} onChange={(e)=> setContent(e.target.value)}></textarea>
+              <textarea {...content}></textarea>
             </div>
 
             <button className="create-post-btn"> Create Post</button>
