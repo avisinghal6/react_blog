4d02f96aa1f6a60c2d852f614628277649b4d8f0
Avi Singhal <avisinghal6@gmail.com>
2022-08-22 01:14:11 -0500

    added comments to postdetail

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -2,7 +2,9 @@ import {useState,useEffect} from 'react';
 import {firestore} from '../firebase';
 import {useParams} from 'react-router-dom';
 function PostDetail() {
-    const [post,setPost] = useState({});
+    const [post,setPost] = useState({}); //useState triggers a rerender when the state changes, thats why hooks are used here
+    // since useEffect runs after first mount thats when post gets its value, and since post is a hook, useState will cause 
+    //rerender when it is assgined a new value.
     const {postId} = useParams(); // used to get the id from the url (/postId)
     useEffect(()=>{
         firestore
